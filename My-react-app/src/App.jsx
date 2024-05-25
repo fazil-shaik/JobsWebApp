a4f4850078c1c7d0801170c15a483228e1a8911c
kali <kali@kali.kali>
2024-05-25 11:40:40 +0530

    changes made in appn.jsx

--- My-react-app/src/App.jsx
+++ My-react-app/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   
   const handleSearch = async () => {
     try {
-      const response = await axios.get('http://127.0.0.1:5000/api/jobs', {
+      const response = await axios.get('https://jobswebapp.onrender.com/api/jobs', {
         params: { searchTerm }
       });
       setJobs(response.data);
